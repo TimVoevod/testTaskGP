ef3c62aa781247794835b12d3f5fefda7573343a
TimVoevod <timvoevod@gmail.com>
2018-01-27 21:51:26 +0300

    final

--- src/testTask557.java
+++ src/testTask557.java
@@ -1,5 +1,5 @@
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,55 +10,51 @@ import java.util.stream.Collectors;
 public class testTask557 {
 
     public static void main(String ... args) throws IOException {
-        try(Scanner scanner = new Scanner(Paths.get("INPUT.TXT"));
-            FileWriter out = new FileWriter("OUTPUT.TXT")){
-            ArrayList<Integer> countAndSize = new ArrayList<>(Arrays.stream(scanner.nextLine()
+        Scanner in = new Scanner(Paths.get("input.txt"));
+        PrintWriter out = new PrintWriter("output.txt");
+            ArrayList<Integer> countAndSize = new ArrayList<>(Arrays.stream(in.nextLine()
                     .split(" "))
                     .map(Integer::parseInt)
                     .collect(Collectors.toList()));
-            int count = countAndSize.get(0);
-            int size = countAndSize.get(1);
-            ArrayList<Integer> lineAndColumn = new ArrayList<>(Arrays.stream(scanner.nextLine()
+            int m = countAndSize.get(0);
+            int n = countAndSize.get(1);
+            ArrayList<Integer> lineAndColumn = new ArrayList<>(Arrays.stream(in.nextLine()
                     .split(" "))
                     .map(Integer::parseInt)
                     .collect(Collectors.toList()));
-            int primeNumber = Arrays.stream(scanner.nextLine()
+            int primeNumber = Arrays.stream(in.nextLine()
                     .split(" "))
                     .map(Integer::parseInt)
                     .collect(Collectors.toList()).get(0);
             ArrayList<Integer> array = new ArrayList<>();
-            scanner.nextLine();
-            for(int i = 0 ; i < size ; i++){
+            in.nextLine();
+            for(int i = 0 ; i < n ; i++){
                 if(i == (lineAndColumn.get(0)-1)) {
-                    array.addAll(Arrays.stream(scanner.nextLine()
+                    array.addAll(Arrays.stream(in.nextLine()
                             .split(" "))
                             .map(Integer::parseInt)
                             .collect(Collectors.toList()));
                 }else{
-                    scanner.nextLine();
+                    in.nextLine();
                 }
             }
-            while(count > 1) {
-                scanner.nextLine();
+            while(m > 1) {
+                in.nextLine();
                 ArrayList<Integer> array1 = new ArrayList<>(Collections.nCopies(countAndSize.get(1),0));
-                for(int i = 0 ; i < size; i++){
-                    ArrayList<Integer> array2 = new ArrayList<>(Arrays.stream(scanner.nextLine()
+                for(int i = 0 ; i < n; i++){
+                    ArrayList<Integer> array2 = new ArrayList<>(Arrays.stream(in.nextLine()
                             .split(" "))
                             .map(Integer::parseInt)
                             .collect(Collectors.toList()));
-                    for (int j = 0; j < size; j++) {
+                    for (int j = 0; j < n; j++) {
                         array1.set(j, (array1.get(j) + array.get(i) * array2.get(j)) % primeNumber);
                     }
                 }
                 Collections.copy(array,array1);
-                count--;
+                m--;
             }
             out.write(String.valueOf(array.get(lineAndColumn.get(1)-1)));
             out.flush();
-        }
-        catch(IOException e){
-
-            e.printStackTrace();
-        }
     }
 }
+
